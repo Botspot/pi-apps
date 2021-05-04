beb067c1321c400e70406d0ca267ebe172ac7a83
tehfrutcak1 <61666366+tehfrutcak1@users.noreply.github.com>
2021-05-03 23:26:41 -0400

    I rewrote and made better the entire project in C
    I removed the bloat and made execution much more efficient by rewriting the entire project in C. I hope you like it! <3

---
+++ source.c
@@ -0,0 +1,6 @@
+
+#include <stdio.h>
+
+int main(void) {
+        printf("Look up the following: \n How to add a repo to sources.list raspberry pi \n <program name> deb package download for arm \n How to use dpkg to install from a deb \n How to compile from source on the Raspberry Pi \n");
+}
