662b7d6d2602fb18d1e03724421ff29e8c450995
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-12-31 01:56:35 -0500

    use the default fallback unless necessary (to avoid code duplication)

--- .github/workflows/updates/Discord.sh
+++ .github/workflows/updates/Discord.sh
@@ -3,7 +3,5 @@
 webVer=$(get_release SpacingBat3/WebCord)
 armhf_url="https://github.com/SpacingBat3/WebCord/releases/download/v${webVer}/webcord_${webVer}_armhf.deb"
 arm64_url="https://github.com/SpacingBat3/WebCord/releases/download/v${webVer}/webcord_${webVer}_arm64.deb"
-pi_apps_ver_32="$(cat 'install-32' | grep 'version=' | sed 's/version=//')"
-pi_apps_ver_64="$(cat 'install-64' | grep 'version=' | sed 's/version=//')"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
