1e1013cf9ae1679cd8f4db498f4741a264a5f227
Sussy Inc <SussyS0S-Operating-Systems@protonmail.com>
2022-02-21 09:50:29 -0500

    Create LineRider.sh

---
+++ .github/workflows/updates/LineRider.sh
@@ -0,0 +1,5 @@
+webVer=$(get_release Sussy-OS/LineRiderUpdater)
+all_url="https://github.com/Sussy-OS/LineRiderUpdater/releases/download/${webVer}/TempFile-${webVer}.txt"
+pi_apps_ver="$(cat 'install' | grep 'version=' | sed 's/version=//')"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
