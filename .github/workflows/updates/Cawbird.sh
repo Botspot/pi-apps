e54b58e023461b74f53e5ddeaeec45aa1363c4b7
cycool29 <cycool29@gmail.com>
2022-03-01 01:01:45 +0800

    Add Cawbird (#1541)
    * Add Cawbird
    
    * Add `gstreamer1.0-libav` as dependency

---
+++ .github/workflows/updates/Cawbird.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+webVer="$(wget -qO- "https://software.opensuse.org//download.html?project=home%3AIBBoard%3Acawbird&package=cawbird" | grep -m 1 "Raspbian.*deb" | sed 's/.*Raspbian_10//g' | grep -o '_.*_' | tr -d '_')"
+armhf_url="https://download.opensuse.org/repositories/home:/IBBoard:/cawbird/Raspbian_10/armhf/cawbird_${webVer}_armhf.deb"
+arm64_url="https://download.opensuse.org/repositories/home:/IBBoard:/cawbird/Raspbian_10/armhf/cawbird_${webVer}_arm64.deb"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
+
