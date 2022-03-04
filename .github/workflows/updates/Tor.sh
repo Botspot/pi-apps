93ab85bf0c6c7fa7c245e70c48aef7d6634844bd
Botspot <54716352+Botspot@users.noreply.github.com>
2022-03-04 14:48:16 -0600

    Fix Tor update script

--- .github/workflows/updates/Tor.sh
+++ .github/workflows/updates/Tor.sh
@@ -2,7 +2,7 @@
 
 
 webVer="$(wget -qO- https://sourceforge.net/projects/tor-browser-ports/files | grep -F 'Download Latest Version' --after 1 | tail -n -1 | tr '<>\-_' '\n' | sed -n 8p)"
-armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${version}/tor-browser-linux-armhf-/${version}_en-US.tar.xz/download"
-arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${version}/tor-browser-linux-armhf-/${version}_en-US.tar.xz/download"
+armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-/${webVer}_en-US.tar.xz/download"
+arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-/${webVer}_en-US.tar.xz/download"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
