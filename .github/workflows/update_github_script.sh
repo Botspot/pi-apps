4dba24b5982b980eed5605aedf8b3820322d1490
cycool29 <cycool29@gmail.com>
2022-03-04 14:36:17 +0800

    Add markdown syntaxes

--- .github/workflows/update_github_script.sh
+++ .github/workflows/update_github_script.sh
@@ -75,10 +75,10 @@ if [ -n "$pi_apps_ver_32" ]  && [ -a "$DIRECTORY/apps/$app_name/install-32" ]; t
         if validate_url "$armhf_url"; then
             status_green "Updating pi-apps $app_name install-32 to: $armhf_url"
             sed -i "0,/version${version_number}=.*/s//version${version_number}=${webVer}/g" install-32
-            echo "$app_name-armhf " >> /tmp/updated_apps
+            echo "- $app_name-armhf " >> /tmp/updated_apps
         else
             warning "Updating $app_name install-32 had been skipped, the upstream file $armhf_url does NOT exist."
-            echo "Updating $app_name install-32 had been skipped, the upstream file $armhf_url does NOT exist." >> /tmp/failed_apps
+            echo "**Updating $app_name install-32 had been skipped, the upstream file $armhf_url does NOT exist.**" >> /tmp/failed_apps
         fi
     fi
 fi
@@ -93,10 +93,10 @@ if [ -n "$pi_apps_ver_64" ] && [ -a "$DIRECTORY/apps/$app_name/install-64" ]; th
         if validate_url "$arm64_url"; then
             status_green "Updating pi-apps $app_name install-64 to: $arm64_url"
             sed -i "0,/version${version_number}=.*/s//version${version_number}=${webVer}/g" install-64
-            echo "$app_name-arm64 " >> /tmp/updated_apps
+            echo "- $app_name-arm64 " >> /tmp/updated_apps
         else
             warning "Updating $app_name install-64 had been skipped, the upstream file $arm64_url does NOT exist."
-            echo "Updating $app_name install-64 had been skipped, the upstream file $arm64_url does NOT exist." >> /tmp/failed_apps
+            echo "**Updating $app_name install-64 had been skipped, the upstream file $arm64_url does NOT exist.**" >> /tmp/failed_apps
         fi
     fi
 fi
@@ -111,10 +111,10 @@ if [ -n "$pi_apps_ver" ] && [ -n "$all_url" ] && [ -a "$DIRECTORY/apps/$app_name
         if validate_url "$all_url"; then
             status_green "Updating pi-apps $app_name install to: $all_url"
             sed -i "0,/version${version_number}=.*/s//version${version_number}=${webVer}/g" install
-            echo "$app_name-all " >> /tmp/updated_apps
+            echo "- $app_name-all " >> /tmp/updated_apps
         else
             warning "Updating $app_name install had been skipped, the upstream file $all_url does NOT exist."
-            echo "Updating $app_name install had been skipped, the upstream file $all_url does NOT exist." >> /tmp/failed_apps
+            echo "**Updating $app_name install had been skipped, the upstream file $all_url does NOT exist.**" >> /tmp/failed_apps
         fi
     fi
 fi
@@ -129,10 +129,10 @@ if [ -n "$pi_apps_ver" ] && [ -n "$armhf_url" ] && [ -n "$arm64_url" ] && [ -a "
         if validate_url "$armhf_url" && validate_url "$arm64_url"; then
             status_green "Updating pi-apps $app_name install to: $armhf_url $arm64_url"
             sed -i "0,/version${version_number}=.*/s//version${version_number}=${webVer}/g" install
-            echo "$app_name-all " >> /tmp/updated_apps
+            echo "- $app_name-all " >> /tmp/updated_apps
         else
             warning "Updating $app_name install had been skipped, the upstream file $armhf_url or $arm64_url does NOT exist."
-            echo "Updating $app_name install had been skipped, the upstream file $armhf_url or $arm64_url does NOT exist." >> /tmp/failed_apps
+            echo "**Updating $app_name install had been skipped, the upstream file $armhf_url or $arm64_url does NOT exist.**" >> /tmp/failed_apps
         fi
     fi
 fi
@@ -141,7 +141,7 @@ done
 else
 
 warning "webVer variable is missing for $app_name update script, please fix this script, skipping update check."
-echo "webVer variable is missing for $app_name update script, please fix this script, skipping update check." >> /tmp/failed_apps
+echo "**webVer variable is missing for $app_name update script, please fix this script, skipping update check.**" >> /tmp/failed_apps
 
 fi
 
