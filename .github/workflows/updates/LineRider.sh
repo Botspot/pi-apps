95deb06ab50213a5a52725a2dfb9f42776058986
Sussy Inc <SussyS0S-Operating-Systems@protonmail.com>
2022-03-05 09:07:21 -0500

    Update LineRider.sh

--- .github/workflows/updates/LineRider.sh
+++ .github/workflows/updates/LineRider.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-webVer=$(get_release Sussy-OS/LineRiderUpdater)
-all_url="https://github.com/Sussy-OS/LineRiderUpdater/releases/download/${webVer}/TempFile-${webVer}.txt"
+webVer=$(get_release Sussy-OS/LRA-Community-Edition)
+all_url="https://github.com/Sussy-OS/LRA-Community-Edition/releases/download/${webVer}/LineRider.game.auto-release.${webVer}-LR.zip"
 pi_apps_ver="$(cat 'install' | grep 'version=' | sed 's/version=//')"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
