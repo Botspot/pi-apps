75bdd8a96f44c89dd3449655730349a4ded50961
Sussy Inc <SussyS0S-Operating-Systems@protonmail.com>
2022-03-06 14:07:26 -0500

    remove junky thing
    Co-authored-by: cycool29 <cycool29@gmail.com>

--- .github/workflows/updates/LineRider.sh
+++ .github/workflows/updates/LineRider.sh
@@ -2,6 +2,5 @@
 
 webVer=$(get_release Sussy-OS/LRA-Community-Edition)
 all_url="https://github.com/Sussy-OS/LRA-Community-Edition/releases/download/${webVer}/LineRider.game.auto-release.${webVer}-LR.zip"
-pi_apps_ver="$(cat 'install' | grep 'version=' | sed 's/version=//')"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
