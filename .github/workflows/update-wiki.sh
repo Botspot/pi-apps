cabdea6fa6e7fc636e3cce271d343780f86b5d60
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-03-07 14:18:37 -0500

    attempt another method for wiki rendering
    hopefully fixes github and https://docs.pi-apps.io/apps-list/ wiki rendering at the same time

--- .github/workflows/update-wiki.sh
+++ .github/workflows/update-wiki.sh
@@ -36,8 +36,8 @@ conversion() {
                 [[ ! -z "$script_website" ]] && [[ -z "$script_credits" ]] && echo "$script_website<br />"
                 [[ -z "$script_website" ]] && [[ ! -z "$script_credits" ]] && echo "$script_credits<br />"
                 echo "$arch"
-                echo '>'
-                cat "$GITHUB_WORKSPACE/apps/$script/description" | sed -e "s/.*/    &/"
+                echo ""
+                cat "$GITHUB_WORKSPACE/apps/$script/description" | sed -e "s/.*/> &/"
             fi
             iter=$(($iter + 1))
         done
