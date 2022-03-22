53314663ec40c5d96eb0f1801e31b3ca9439488d
Jai-JAP <jai.jap.318@gmail.com>
2022-03-22 16:16:37 +0530

    Oops

--- .github/workflows/updates/Tabby.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-webVer=$(get_release Jai-JAP/tabby-arm-builds)
-arm64_url="https://github.com/Jai-JAP/tabby-arm-builds/releases/download/v${version}/tabby-${version}-linux.deb"
-
-source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
