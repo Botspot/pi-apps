a764ed1553129a5a85cf7897da5657ac28443176
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-03-22 17:42:28 -0400

    add system-monitoring-center

---
+++ .github/workflows/updates/System Monitoring Center.sh
@@ -0,0 +1,4 @@
+webVer=$(get_release hakandundar34coding/system-monitoring-center)
+all_url="https://github.com/hakandundar34coding/system-monitoring-center/releases/download/v${webVer}/system-monitoring-center_${webVer}_all.deb"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
