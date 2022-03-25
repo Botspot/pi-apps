356bdde067e205baa4c79221bb39ac25f0e809fe
Jai-JAP <jai.jap.318@gmail.com>
2022-03-25 15:54:32 +0530

    Armv7l

--- .github/workflows/updates/Tabby.sh
+++ .github/workflows/updates/Tabby.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 webVer=$(get_release Jai-JAP/tabby-arm-builds)
-arm64_url="https://github.com/Jai-JAP/tabby-arm-builds/releases/download/v${version}/tabby-${version}-linux.deb"
+armhf_url="https://github.com/Jai-JAP/tabby-arm-builds/releases/download/v${version}/tabby-${version}-linux_armv7l.deb"
+arm64_url="https://github.com/Jai-JAP/tabby-arm-builds/releases/download/v${version}/tabby-${version}-linux_arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
