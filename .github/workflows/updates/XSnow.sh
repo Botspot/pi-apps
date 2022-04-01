116a1d169ce3f50fa8bc9c841508646ece2c1830
cycool29 <cycool29@gmail.com>
2022-04-01 08:56:51 +0800

    Fix typo

--- .github/workflows/updates/XSnow.sh
+++ .github/workflows/updates/XSnow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(wget -qO- https://www.ratrabbit.nl/ratrabbit/xsnow/downloads/index.html | grep "Download debian package for Raspbian:"  | sed 's/<p>//g ; s+</p>++g' | sed 's+Download debian package for Raspbian (bullseye): <a href="./../../../downloads/xsnow/.*armhf.deb">++g ; s+</a>++g' | grep -oP '^[^\_]*\_\K[^\_]+')
+webVer=$(wget -qO- https://www.ratrabbit.nl/ratrabbit/xsnow/downloads/index.html | grep "Download debian package for Raspbian:"  | sed 's/<p>//g ; s+</p>++g' | sed 's+Download debian package for Raspbian: <a href="./../../../downloads/xsnow/.*armhf.deb">++g ; s+</a>++g' | grep -oP '^[^\_]*\_\K[^\_]+')
 armhf_url="https://www.ratrabbit.nl/downloads/xsnow/xsnow_${webVer}_armhf.deb"
 arm64_url="https://www.ratrabbit.nl/downloads/xsnow/xsnow_${webVer}_arm64.deb"
 
