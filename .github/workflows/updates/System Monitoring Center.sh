8336faf4828c982d82b5ac5b4295bb4a5dbf56ef
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-04-09 11:57:25 -0400

    correct download link and automated script

--- .github/workflows/updates/System Monitoring Center.sh
+++ .github/workflows/updates/System Monitoring Center.sh
@@ -1,4 +1,4 @@
-webVer=$(get_release hakandundar34coding/system-monitoring-center)
-all_url="https://github.com/hakandundar34coding/system-monitoring-center/releases/download/v${webVer}/system-monitoring-center_${webVer}_all.deb"
+webVer=$(get_release hakandundar34coding/system-monitoring-center | sed 's/[^0-9.]*//g')
+all_url="https://github.com/hakandundar34coding/system-monitoring-center/releases/download/v${webVer}-deb_for_stores/system-monitoring-center_${webVer}_all.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
