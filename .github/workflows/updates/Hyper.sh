f5dd34f9542bbc25c9c040d222cc79dda56b108e
Crilum <91354257+Crilum@users.noreply.github.com>
2022-04-30 22:37:54 -0600

    Update Hyper.sh
    Revert updater to @Jai-JAP's builds, fix wrong variables

--- .github/workflows/updates/Hyper.sh
+++ .github/workflows/updates/Hyper.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 webVer=$(get_release Jai-JAP/hyper-arm-builds)
-armhf_url="https://github.com/Jai-JAP/hyper-arm-builds/releases/download/v${version}/hyper_${version}_armv7l.deb"
+armhf_url="https://github.com/Jai-JAP/hyper-arm-builds/releases/download/v${webVer}hyper_${webVer}_armv7l.deb"
+arm64_url="https://github.com/Jai-JAP/hyper-arm-builds/releases/download/v${version}/hyper_${version}_arm64.deb"
 
-source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
-
-unset webVer
-unset armhf_url
-webVer=$(get_release vercel/hyper)
-arm64_url="https://github.com/vercel/hyper/releases/download/v${version}/hyper_${version}_arm64.deb"
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
