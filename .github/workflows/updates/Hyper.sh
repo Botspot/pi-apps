656c0d3467dc80fa42eea6b44d284a545dda3133
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-05-02 00:05:28 -0400

    correct hyper arm64_url

--- .github/workflows/updates/Hyper.sh
+++ .github/workflows/updates/Hyper.sh
@@ -2,6 +2,6 @@
 
 webVer=$(get_release Jai-JAP/hyper-arm-builds)
 armhf_url="https://github.com/Jai-JAP/hyper-arm-builds/releases/download/v${webVer}hyper_${webVer}_armv7l.deb"
-arm64_url="https://github.com/Jai-JAP/hyper-arm-builds/releases/download/v${version}/hyper_${version}_arm64.deb"
+arm64_url="https://github.com/Jai-JAP/hyper-arm-builds/releases/download/v${webVer}/hyper_${webVer}_arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
