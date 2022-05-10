314ff8ff33ab105f95eac82402d48150b046b743
cycool29 <cycool29@gmail.com>
2022-05-10 14:30:21 +0800

    Change Notepad++ update script to fetch version from official website

--- .github/workflows/updates/Notepad ++.sh
+++ .github/workflows/updates/Notepad ++.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(get_release notepad-plus-plus/notepad-plus-plus)
+webVer=$(wget -qO- https://notepad-plus-plus.org/ | grep 'Current Version' | sed 's/.*Current Version //g' | sed 's+<.*++g')
 armhf_url="https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v${webVer}/npp.${webVer}.portable.zip"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
