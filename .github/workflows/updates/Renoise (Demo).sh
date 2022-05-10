2cf4f9ccb25eb402387b980fb7ca57fb16b8277e
Crilum <91354257+Crilum@users.noreply.github.com>
2022-05-09 21:01:35 -0600

    Good update script

--- .github/workflows/updates/Renoise (Demo).sh
+++ .github/workflows/updates/Renoise (Demo).sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo "Checking Renoise version and getting download urls..."
+echo "Checking Renoise version and getting download urls (This will take a long time)..."
 
 for (( i=999; i > 0; i-- )); do
 	webVer="$(echo "$i" | sed -e 's/^\(.\{1\}\)/\1_/' -e 's/^\(.\{3\}\)/\1_/')"
         armhf_url="https://files.renoise.com/demo/Renoise_${webVer}_Demo_Linux_armhf.tar.gz"
         arm64_url="https://files.renoise.com/demo/Renoise_${webVer}_Demo_Linux_arm64.tar.gz"
-        wget --spider $armhf_url &>/dev/null && break
-	echo $armhf_url
+	wget --spider $armhf_url &>/dev/null && break
 done
 
 echo "Double-checking urls..."
@@ -24,7 +23,6 @@ else
         echo "\$armhf_url ($armhf_url) checks out! We're good to go."
 fi
 
-webVer="${base_ver}${minor_ver}"
 echo "Latest version: ${webVer}"
 
 source ${GITHUB_WORKSPACE}/.github/workflows/update_github_script.sh
