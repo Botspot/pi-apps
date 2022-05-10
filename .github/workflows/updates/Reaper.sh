8ffad9414f0e3db8dc85231719a1ac0a7c310d60
Crilum <91354257+Crilum@users.noreply.github.com>
2022-05-09 19:28:50 -0600

    Add Reaper update script, and add ReaPack to Reaper

---
+++ .github/workflows/updates/Reaper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Checking Reaper major version..."
+for (( i=0; i < 10; i++ )); do
+        base_ver_test_url="https://reaper.fm/files/${i}.x/reaper${i}01_linux_armv7l.tar.xz"
+	base_ver="${i}"
+        wget --spider $base_ver_test_url &>/dev/null && break
+done
+
+echo "Checking Reaper minor version and getting download urls..."
+
+for (( i=99; i > 0; i-- )); do
+	armhf_url="https://reaper.fm/files/${base_ver}.x/reaper${base_ver}${i}_linux_armv7l.tar.xz"
+	arm64_url="https://reaper.fm/files/${base_ver}.x/reaper${base_ver}${i}_linux_aarch64.tar.xz"
+	wget --spider $armhf_url &>/dev/null && break
+done
+
+echo "Double-checking urls..."
+
+if ! wget --spider ${arm64_url} &>/dev/null; then
+	echo "\$arm64_url ($arm64_url) isn't good! Something bad happened, not updating arm64..."; unset arm64_url
+else
+	echo "\$arm64_url ($arm64_url) checks out! We're good to go."
+fi
+
+if ! wget --spider ${armhf_url} &>/dev/null; then
+        echo "\$armhf_url ($armhf_url) isn't good! Something bad happened, not updating armhf..."; unset armhf_url
+else
+        echo "\$armhf_url ($armhf_url) checks out! We're good to go."
+fi
+
+source ${GITHUB_WORKSPACE}/.github/workflows/update_github_script.sh
