9e503525a6742ab74b909339218f1c6704f7f8e3
Crilum <91354257+Crilum@users.noreply.github.com>
2022-05-09 21:09:29 -0600

    Add reapack updates

--- .github/workflows/updates/Reaper.sh
+++ .github/workflows/updates/Reaper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "Checking Reaper major version..."
 for (( i=0; i < 10; i++ )); do
         base_ver_test_url="https://reaper.fm/files/${i}.x/reaper${i}01_linux_armv7l.tar.xz"
@@ -9,8 +10,8 @@ done
 echo "Checking Reaper minor version and getting download urls..."
 
 for (( i=99; i > 0; i-- )); do
-	armhf_url="https://reaper.fm/files/${base_ver}.x/reaper${base_ver}${i}_linux_armv7l.tar.xz"
-	arm64_url="https://reaper.fm/files/${base_ver}.x/reaper${base_ver}${i}_linux_aarch64.tar.xz"
+	armhf_url[0]="https://reaper.fm/files/${base_ver}.x/reaper${base_ver}${i}_linux_armv7l.tar.xz"
+	arm64_url[0]="https://reaper.fm/files/${base_ver}.x/reaper${base_ver}${i}_linux_aarch64.tar.xz"
 	minor_ver="${i}"
 	wget --spider $armhf_url &>/dev/null && break
 done
@@ -29,7 +30,11 @@ else
         echo "\$armhf_url ($armhf_url) checks out! We're good to go."
 fi
 
-webVer="${base_ver}${minor_ver}"
+webVer[0]="${base_ver}${minor_ver}"
 echo "Latest version: ${webVer}"
+
+webVer[1]="$(get_release cfillion/reapack)"
+armhf_url[1]="https://github.com/cfillion/reapack/releases/download/v${webVer}/reaper_reapack-armv7l.so"
+arm64_url[1]="https://github.com/cfillion/reapack/releases/download/v${webVer}/reaper_reapack-aarch64.so"
 
 source ${GITHUB_WORKSPACE}/.github/workflows/update_github_script.sh
