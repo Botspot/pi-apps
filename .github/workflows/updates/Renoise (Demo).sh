3dd8b90b55d99a1f3cf4b85830d23189c0cebd82
Crilum <91354257+Crilum@users.noreply.github.com>
2022-05-10 13:39:38 -0600

    Revert "Make url getting more future-proof"
    This reverts commit 6b806702e830c8abc314a63b45e89aeaac1ee087.

--- .github/workflows/updates/Renoise (Demo).sh
+++ .github/workflows/updates/Renoise (Demo).sh
@@ -3,9 +3,8 @@
 echo "Checking Renoise version and getting download urls..."
 
 webVer=$(curl -s https://files.renoise.com/demo/ | grep "armhf" | sed -e 's/<a href="//' -e 's/".*//' -e 's/Renoise_//' -e 's/_Demo_Linux_armhf.tar.gz//')
-
-armhf_url="https://renoise.com/demo/$(curl -s https://files.renoise.com/demo/ | grep "armhf" | sed -e 's/<a href="//' -e 's/".*//')"
-arm64_url="https://renoise.com/demo/$(curl -s https://files.renoise.com/demo/ | grep "arm64" | sed -e 's/<a href="//' -e 's/".*//')"
+armhf_url="https://renoise.com/demo/Renoise_${webVer}_Demo_Linux_armhf.tar.gz"
+arm64_url="https://renoise.com/demo/Renoise_${webVer}_Demo_Linux_arm64.tar.gz"
 echo "Double-checking urls..."
 
 if ! wget --spider ${arm64_url} &>/dev/null; then
