845676f6f0d4c9dbdc72a32c7cc486e9cfa2b08a
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-05-11 06:42:17 -0700

    improve renoise updater
    * add missing quotation marks
    * remove unnecessary line

--- .github/workflows/updates/Renoise (Demo).sh
+++ .github/workflows/updates/Renoise (Demo).sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "Checking Renoise version and getting download urls..."
-
-webVer=$(curl -s https://files.renoise.com/demo/ | grep "armhf" | sed -e 's/<a href="//' -e 's/".*//' -e 's/Renoise_//' -e 's/_Demo_Linux_armhf.tar.gz//')
+webVer="$(curl -s https://files.renoise.com/demo/ | grep "armhf" | sed -e 's/<a href="//' -e 's/".*//' -e 's/Renoise_//' -e 's/_Demo_Linux_armhf.tar.gz//')g"
 armhf_url="https://renoise.com/demo/Renoise_${webVer}_Demo_Linux_armhf.tar.gz"
 arm64_url="https://renoise.com/demo/Renoise_${webVer}_Demo_Linux_arm64.tar.gz"
 
