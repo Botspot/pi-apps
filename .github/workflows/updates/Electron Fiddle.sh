b8a1a86ab6c98a7539ebb955341f591eeceba20b
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-05-20 22:10:15 -0400

    correct electron fiddle updater

--- .github/workflows/updates/Electron Fiddle.sh
+++ .github/workflows/updates/Electron Fiddle.sh
@@ -2,6 +2,6 @@
 
 webVer=$(get_release electron/fiddle)
 armhf_url="https://github.com/electron/fiddle/releases/download/v${webVer}/electron-fiddle_${webVer}_armhf.deb"
-arm64_url="https://github.com/electron/fiddle/releases/download/v${webVer}/electron/fiddle/electron-fiddle_${webVer}_arm64.deb"
+arm64_url="https://github.com/electron/fiddle/releases/download/v${webVer}/electron-fiddle_${webVer}_arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
