4969b8ac9e6d9f5142365e195f0745655714344b
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-05-20 22:13:06 -0400

    update veracrypt user agent
    this randomly fails.. I assume its because sourceforge is denying access to github for downloading the version

--- .github/workflows/updates/VeraCrypt.sh
+++ .github/workflows/updates/VeraCrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer="$(wget --header="Accept: text/html" --user-agent="Linux" -qO- https://sourceforge.net/projects/veracrypt/files/ | sed -n '/Latest/{n;p;}' | sed 's/.*veracrypt-//g; s/-setup.*//g')"
+webVer="$(wget --header="Accept: text/html" --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36" -qO- https://sourceforge.net/projects/veracrypt/files/ | sed -n '/Latest/{n;p;}' | sed 's/.*veracrypt-//g; s/-setup.*//g')"
 armhf_url="https://sourceforge.net/projects/veracrypt/files/VeraCrypt%20${webVer}/Linux/veracrypt-${webVer}-Debian-10-armhf.deb"
 arm64_url="https://sourceforge.net/projects/veracrypt/files/VeraCrypt%20${webVer}/Linux/veracrypt-${webVer}-Debian-10-arm64.deb"
 
