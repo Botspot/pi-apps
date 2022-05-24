95f94b750ec098975ed199855ac555f7fb0257f6
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-05-23 23:31:24 -0400

    add worldpainter app
    closes https://github.com/Botspot/pi-apps/issues/919

---
+++ .github/workflows/updates/WorldPainter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+webVer="$(wget -qO- https://www.worldpainter.net/ | grep -m 1 "worldpainter_.*.deb" | grep -o '_.*.deb' | tr -d '_' | sed 's/.deb//')"
+all_url="https://www.worldpainter.net/files/worldpainter_${WebVer}.deb"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
