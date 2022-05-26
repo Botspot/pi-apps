4ab46a747170d6260fe3bead5f910aa1c6477a9d
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-05-25 23:10:02 -0400

    the WPS server has one of the world response rates ever
    add a 5 second timeout so that the script doesn't hang wait for the automatic 6 hour github actions killer

--- .github/workflows/updates/WPS Office.sh
+++ .github/workflows/updates/WPS Office.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer="$(wget -qO- https://linux.wps.cn/ | grep -o 'wps\-office_.*_arm64\.deb' | sed 's/^wps\-office_//g' | sed 's/_arm64\.deb//g')"
+webVer="$(wget -qO- --timeout=5 https://linux.wps.cn/ | grep -o 'wps\-office_.*_arm64\.deb' | sed 's/^wps\-office_//g' | sed 's/_arm64\.deb//g')"
 arm64_url="https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/$(echo "$webVer" | sed 's/.*\.//g')/wps-office_${webVer}_arm64.deb"
 armhf_url="https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/$(echo "$webVer" | sed 's/.*\.//g')/wps-office_${webVer}_arm64.deb"
 
