39cbc6532def1605f189034f8a58a0a3b32045ed
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-05-30 23:16:35 -0400

    switch to Davidk prusaslicer builds
    add auto updater as well

---
+++ .github/workflows/updates/PrusaSlicer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+webVer=$(get_release davidk/PrusaSlicer-ARM.AppImage | tr -d "version_")
+armhf_url="https://github.com/davidk/PrusaSlicer-ARM.AppImage/releases/download/version_${webVer}/PrusaSlicer-version_${webVer}-armhf.AppImage"
+arm64_url="https://github.com/davidk/PrusaSlicer-ARM.AppImage/releases/download/version_${webVer}/PrusaSlicer-version_${webVer}-aarch64.AppImage"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
