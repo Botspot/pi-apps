9df721f494d92af3138075d55bc04a58b61ab63c
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-06-23 10:45:14 -0400

    Min fix updater

--- .github/workflows/updates/Min.sh
+++ .github/workflows/updates/Min.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 webVer=$(get_release minbrowser/min)
-armhf_url="https://github.com/minbrowser/min/releases/download/v${webVer}/min-${webVer}-armhf.deb"
+armhf_url="https://github.com/minbrowser/min/releases/download/v${webVer}/min-${webVer}-armv7l.deb"
 arm64_url="https://github.com/minbrowser/min/releases/download/v${webVer}/min-${webVer}-arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
