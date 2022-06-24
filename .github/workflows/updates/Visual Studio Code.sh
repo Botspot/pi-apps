6bc1c72f7060f92cf9dea9932051614ab0603437
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-06-24 12:11:00 -0400

    Revert "Make VSCode's deb URL hardcoded (#2008)"
    This reverts commit 56ba412d9189048823ab4e5838375e473743f306.

--- .github/workflows/updates/Visual Studio Code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-webVer="$(curl -sLI -o /dev/null -w %{url_effective} https://aka.ms/linux-armhf-deb | grep -o '_.*-' | sed 's/_\|-//g')"
-armhf_url="https://update.code.visualstudio.com/${webVer}/linux-deb-armhf/stable"
-arm64_url="https://update.code.visualstudio.com/${webVer}/linux-deb-arm64/stable"
-
-source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
