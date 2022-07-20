f6bf783e711054afab3488b018c2c727ced7e552
Botspot <54716352+Botspot@users.noreply.github.com>
2022-07-20 13:25:40 -0500

    Update Sonic Pi to bullseye debs (#2065)
    * update to bullseye debs
    
    * Correct file permissions for install and uninstall files
    
    * add "to run"
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- .github/workflows/updates/Sonic Pi.sh
+++ .github/workflows/updates/Sonic Pi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-webVer=$(curl -s https://sonic-pi.net | grep -m 1 armhf | sed 's/.*sonic-pi_//g; s/_armhf.*//g')
-armhf_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_armhf.deb"
+webVer=$(curl -s https://sonic-pi.net | grep -m 1 armhf | sed 's/.*sonic-pi_//g; s/_bullseye\.armhf.*//g')
+armhf_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_bullseye.armhf.deb"
+arm64_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_bullseye.arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
