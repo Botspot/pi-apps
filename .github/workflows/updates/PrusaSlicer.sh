caad368dc19028823f9419fd9f9aa906b9501c57
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-08-23 21:56:45 -0400

    correct prusaslicer updater
    the character v is removed from the github release tag name when using get_release. instead of removing all matched charaters, remove the specific string remaining

--- .github/workflows/updates/PrusaSlicer.sh
+++ .github/workflows/updates/PrusaSlicer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(get_release davidk/PrusaSlicer-ARM.AppImage | tr -d "version_")
+webVer=$(get_release davidk/PrusaSlicer-ARM.AppImage | sed "s/ersion_//g")
 armhf_url="https://github.com/davidk/PrusaSlicer-ARM.AppImage/releases/download/version_${webVer}/PrusaSlicer-version_${webVer}-armhf.AppImage"
 arm64_url="https://github.com/davidk/PrusaSlicer-ARM.AppImage/releases/download/version_${webVer}/PrusaSlicer-version_${webVer}-aarch64.AppImage"
 
