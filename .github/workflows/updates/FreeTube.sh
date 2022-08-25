c0c69fcf9e241733d083a33427657b4f49e12c82
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-08-24 21:13:04 -0400

    Update FreeTube.sh

--- .github/workflows/updates/FreeTube.sh
+++ .github/workflows/updates/FreeTube.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 webVer=$(get_prerelease FreeTubeApp/FreeTube)
-arm64_url="https://github.com/FreeTubeApp/FreeTube/releases/download/v${webVer}/freetube_${webVer-beta}_arm64.deb"
+arm64_url="https://github.com/FreeTubeApp/FreeTube/releases/download/v${webVer}/freetube_${webVer%-beta}_arm64.deb"
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
