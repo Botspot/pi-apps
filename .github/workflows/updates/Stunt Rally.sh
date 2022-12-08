4b4877e50e22bd2936ecff9a2ea1fa8d92152dd1
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-12-07 19:35:55 -0500

    Migrate all raspbian-addons packages (#2193)
    * Switch antimicrox to pi-apps-coders debs
    
    * obs-studio buster: use pi-apps-coders pkgs
    
    * Snapdrop: switch pkg location
    
    * tetris-cli: use pi-apps-coders debs, disable updater
    
    * stuntrally: use pi-apps-coders pkgs, disable updater
    
    * sysmontask: use official debs, disable updater
    
    * keepassxc: use pi-apps-coders pkgs, disable updater
    
    * disable raspbian addons specific updater
    
    Co-authored-by: theofficialgman <28281419+theofficialgman@users.noreply.github.com>

--- .github/workflows/updates/Stunt Rally.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-webVer="$(wget -qO- https://apt.raspbian-addons.org/debian/pool/main/s/stuntrally/ | grep -m 1 "stuntrally.*armhf.deb" | grep -o '_.*_' | tr -d '_')"
-armhf_url="https://apt.raspbian-addons.org/debian/pool/main/s/stuntrally/stuntrally_${webVer}_armhf.deb"
-
-source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
