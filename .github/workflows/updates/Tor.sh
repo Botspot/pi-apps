8483e29da6cca48c0ea7eaae9ac2b3f7d42f9d31
Seb <69688534+SebTNT@users.noreply.github.com>
2023-01-04 15:13:54 +0000

    fixed update for tor

--- .github/workflows/updates/Tor.sh
+++ .github/workflows/updates/Tor.sh
@@ -2,7 +2,11 @@
 
 
 webVer="$(wget -qO- https://sourceforge.net/projects/tor-browser-ports/files | grep -F 'Download Latest Version' --after 1 | tail -n -1 | tr '<>\-_' '\n' | sed -n 8p)"
-armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}_en-US.tar.xz/download"
-arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}_en-US.tar.xz/download"
+armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}_ALL.tar.xz/download"
+arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}_ALL.tar.xz/download"
+
+if [ -d ~/tor-browser/ ];then
+  mv ~/tor-browser ~/tor-browser_en-US
+fi
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
