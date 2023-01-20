483a9bd04511a2013941c0974c3b125a6862c9f2
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-01-20 16:57:56 -0500

    use get_release_raw for nodejs updater

--- .github/workflows/updates/Node.js.sh
+++ .github/workflows/updates/Node.js.sh
@@ -4,7 +4,7 @@ webVer[0]=$(wget -qO- https://nodejs.org/en/download/current/ | grep 'https://no
 armhf_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-armv7l.tar.xz"
 arm64_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-arm64.tar.xz"
 
-webVer[1]=$(get_release nvm-sh/nvm)
+webVer[1]=$(get_release_raw nvm-sh/nvm)
 all_url[1]="https://raw.githubusercontent.com/nvm-sh/nvm/${webVer[1]}/install.sh"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
