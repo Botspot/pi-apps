0de0e625f718e321b40dd31d10d519fcfac75c73
Botspot <54716352+Botspot@users.noreply.github.com>
2023-01-20 14:05:41 -0600

    Make Node.js auto-update

---
+++ .github/workflows/updates/Node.js.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+webVer[0]=$(wget -qO- https://nodejs.org/en/download/current/ | grep 'https://nodejs\.org/dist/.*\.tar\.gz' -m1 | tr '/' '\n' | grep '^v')
+armhf_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-armv7l.tar.xz"
+arm64_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-arm64.tar.xz"
+
+webVer[1]=$(get_release nvm-sh/nvm)
+all_url[1]="https://raw.githubusercontent.com/nvm-sh/nvm/${webVer[1]}/install.sh"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
