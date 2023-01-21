3332db14edcbd1e5e91c6231bfb7876417a9f0d4
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-01-20 19:11:36 -0500

    XSnow: switch to buildscript
    progress for: https://github.com/Botspot/pi-apps/issues/2160
    progress for: https://github.com/Botspot/pi-apps/issues/2232

--- .github/workflows/updates/XSnow.sh
+++ .github/workflows/updates/XSnow.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-webVer=$(wget -qO- https://www.ratrabbit.nl/ratrabbit/xsnow/downloads/index.html | grep -A1 "Download debian package for Raspbian:" | tail -n 1  | sed 's/<p>//g ; s+</p>++g' | sed 's+"./../../../downloads/xsnow/.*armhf.deb">++g ; s+</a>++g' | grep -oP '^[^\_]*\_\K[^\_]+')
-armhf_url="https://www.ratrabbit.nl/downloads/xsnow/xsnow_${webVer}_armhf.deb"
-arm64_url="https://www.ratrabbit.nl/downloads/xsnow/xsnow_${webVer}_arm64.deb"
+webVer=$(wget -qO- https://www.ratrabbit.nl/ratrabbit/xsnow/downloads/index.html | grep -A1 "Download source:" | tail -n 1  | sed 's/<p>//g ; s+</p>++g' | sed 's+"./../../../downloads/xsnow/xsnow-.*tar.gz">++g ; s+</a>++g' | grep -oP '^[^-]*\-\K[^*]+' | sed -n 's/.tar.gz//p')
+all_url="https://www.ratrabbit.nl/downloads/xsnow/xsnow-${webVer}.tar.gz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
