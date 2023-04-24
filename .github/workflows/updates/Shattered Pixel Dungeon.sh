d416e98e5138c425fb26f757380a94e28e0fc5e2
Crilum <91354257+Crilum@users.noreply.github.com>
2023-04-24 14:07:12 -0600

    Shattered Pixel Dungeon: change release filename
    Fix https://discord.com/channels/770629697909424159/770629878319022090/1100139724099813466
    
    Upstream changed the filename one month after posting a release. This would have been caught by the auto-updater on Wednesday but a user noticed it first.

--- .github/workflows/updates/Shattered Pixel Dungeon.sh
+++ .github/workflows/updates/Shattered Pixel Dungeon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 webVer=$(get_release 00-Evan/shattered-pixel-dungeon)
-all_url="https://github.com/00-Evan/shattered-pixel-dungeon/releases/download/v${webVer}/ShatteredPD-v${webVer}-Desktop.jar"
+all_url="https://github.com/00-Evan/shattered-pixel-dungeon/releases/download/v${webVer}/ShatteredPD-v${webVer}-Java.jar"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
