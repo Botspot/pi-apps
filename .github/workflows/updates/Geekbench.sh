9c088d23d84984f2aec1589cc8163a632a2a3d3f
Botspot <54716352+Botspot@users.noreply.github.com>
2023-05-24 17:27:43 -0500

    Fix Geekbench autoupdater

--- .github/workflows/updates/Geekbench.sh
+++ .github/workflows/updates/Geekbench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(wget -qO- https://www.geekbench.com/download/linux/ | grep "You are downloading" | sed "s/.*You are downloading Geekbench //g; s/ for Linux.*//g")
+webVer=$(wget -qO- https://www.geekbench.com/download/linux/ | grep "for Linux. If your download does not start in"  | sed "s/ for Linux.*//g ; s/.* //g")
 arm64_url="https://cdn.geekbench.com/Geekbench-${webVer}-LinuxARMPreview.tar.gz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
