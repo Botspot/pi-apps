697c62086a25ccd28cf1a5eef2a50e3ebf556909
Botspot <54716352+Botspot@users.noreply.github.com>
2023-07-12 09:44:30 -0500

    fix Tor.sh to verify arm64 download

--- .github/workflows/updates/Tor.sh
+++ .github/workflows/updates/Tor.sh
@@ -3,6 +3,6 @@
 
 webVer="$(wget -qO- https://sourceforge.net/projects/tor-browser-ports/files | grep -F 'Download Latest Version' --after 1 | tail -n -1 | tr '<>\-_' '\n' | sed -n 8p)"
 armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}_ALL.tar.xz/download"
-arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}_ALL.tar.xz/download"
+arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-arm64-${webVer}_ALL.tar.xz/download"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
