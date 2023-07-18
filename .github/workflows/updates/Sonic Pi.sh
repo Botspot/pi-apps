9a0a87ccd68091343724fa04841c417b50622ff2
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-07-18 18:57:14 -0400

    Sonic Pi: update updater to use arm64.deb during match
    also remove armhf from updater as upstream has dropped it

--- .github/workflows/updates/Sonic Pi.sh
+++ .github/workflows/updates/Sonic Pi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-webVer=$(curl -s https://sonic-pi.net | grep -m 1 armhf | sed 's/.*sonic-pi_//g; s/_bullseye\.armhf.*//g')
-armhf_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_bullseye.armhf.deb"
+webVer=$(curl -s https://sonic-pi.net | grep -m 1 arm64.deb | sed 's/.*sonic-pi_//g; s/_bullseye\.arm64.*//g')
 arm64_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_bullseye.arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
