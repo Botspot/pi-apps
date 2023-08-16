8344b2d6217de6e14d9e5ab492b49303284f5570
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-08-16 16:15:44 -0400

    System Monitoring Center: updater obtain latest v1 release branch version

--- .github/workflows/updates/System Monitoring Center.sh
+++ .github/workflows/updates/System Monitoring Center.sh
@@ -1,4 +1,4 @@
-webVer=$(get_release hakandundar34coding/system-monitoring-center | sed 's/[^0-9.]*//g')
+webVer=$(curl -s --header "Authorization: token ${{ secrets.GH_PERSONAL_ACCESS_TOKEN }}" "https://api.github.com/repos/hakandundar34coding/system-monitoring-center/releases" | jq -r 'map(select(.prerelease==false)) | map(select(.draft==false)) | map(select(.tag_name | startswith("v1."))) | first | .tag_name' | sed s/^v//g)
 all_url="https://github.com/hakandundar34coding/system-monitoring-center/releases/download/v${webVer}/system-monitoring-center_${webVer}_all.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
