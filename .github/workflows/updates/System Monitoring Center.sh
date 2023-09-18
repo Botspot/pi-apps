83b987b8c9a0a305c16c002c4bb3cf7ecdd9b860
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-09-17 21:51:46 -0400

    System Monitoring Center: update to 2.22.1
    also switch version and version2 variables since CI at the analytics repo checks version only to report the version and we want to report the latest version for any other projects that read our package_data.json file

--- .github/workflows/updates/System Monitoring Center.sh
+++ .github/workflows/updates/System Monitoring Center.sh
@@ -1,6 +1,6 @@
-webVer[0]=$(curl -s --header "Authorization: token $GH_PERSONAL_ACCESS_TOKEN" "https://api.github.com/repos/hakandundar34coding/system-monitoring-center/releases" | jq -r 'map(select(.prerelease==false)) | map(select(.draft==false)) | map(select(.tag_name | startswith("v1."))) | first | .tag_name' | sed s/^v//g)
+webVer[0]=$(curl -s --header "Authorization: token $GH_PERSONAL_ACCESS_TOKEN" "https://api.github.com/repos/hakandundar34coding/system-monitoring-center/releases" | jq -r 'map(select(.prerelease==false)) | map(select(.draft==false)) | map(select(.tag_name | startswith("v2."))) | first | .tag_name' | sed s/^v//g)
 all_url[0]="https://github.com/hakandundar34coding/system-monitoring-center/releases/download/v${webVer[0]}/system-monitoring-center_${webVer[0]}_all.deb"
-webVer[1]=$(curl -s --header "Authorization: token $GH_PERSONAL_ACCESS_TOKEN" "https://api.github.com/repos/hakandundar34coding/system-monitoring-center/releases" | jq -r 'map(select(.prerelease==false)) | map(select(.draft==false)) | map(select(.tag_name | startswith("v2."))) | first | .tag_name' | sed s/^v//g)
+webVer[1]=$(curl -s --header "Authorization: token $GH_PERSONAL_ACCESS_TOKEN" "https://api.github.com/repos/hakandundar34coding/system-monitoring-center/releases" | jq -r 'map(select(.prerelease==false)) | map(select(.draft==false)) | map(select(.tag_name | startswith("v1."))) | first | .tag_name' | sed s/^v//g)
 all_url[1]="https://github.com/hakandundar34coding/system-monitoring-center/releases/download/v${webVer[1]}/system-monitoring-center_${webVer[1]}_all.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
