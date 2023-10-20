83c180461d0f74abb324a71661fbb766ae72fbfd
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-10-20 19:09:35 -0400

    VeraCrypt: update to 1.26.7
    manually package for bookworm due to upstream multi-week delay https://github.com/veracrypt/VeraCrypt/issues/1046#issuecomment-1756614673
    
    hopefully upstream is able to package by the next stable cut
    
    also switch to github releases instead of curseforge

--- .github/workflows/updates/VeraCrypt.sh
+++ .github/workflows/updates/VeraCrypt.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-webVer="$(wget --header="Accept: text/html" --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36" -qO- https://sourceforge.net/projects/veracrypt/files/ | sed -n '/Latest/{n;p;}' | sed 's/.*veracrypt-//g; s/-setup.*//g')"
-armhf_url="https://sourceforge.net/projects/veracrypt/files/VeraCrypt%20${webVer}/Linux/veracrypt-${webVer}-Debian-10-armhf.deb"
-arm64_url="https://sourceforge.net/projects/veracrypt/files/VeraCrypt%20${webVer}/Linux/veracrypt-${webVer}-Debian-10-arm64.deb"
+webVer[0]="$(get_release_raw veracrypt/VeraCrypt | sed 's/VeraCrypt_//g' )"
+webVer[1]="${webVer[0]}"
+armhf_url[0]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[0]}/veracrypt-${webVer[0]}-Debian-10-armhf.deb"
+arm64_url[0]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[0]}/veracrypt-${webVer[0]}-Debian-10-arm64.deb"
+armhf_url[1]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[1]}/veracrypt-${webVer[1]}-Debian-11-armhf.deb"
+arm64_url[1]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[1]}/veracrypt-${webVer[1]}-Debian-11-arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
