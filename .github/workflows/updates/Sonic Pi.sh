c81a49b03acec2c3c78f598675396e17496b34b0
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-11-25 22:39:36 -0500

    Sonic Pi: update to latest bookworm version
    correct auto-update script to latest bookworm changes

--- .github/workflows/updates/Sonic Pi.sh
+++ .github/workflows/updates/Sonic Pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(curl -s https://sonic-pi.net | grep -m 1 arm64.deb | sed 's/.*sonic-pi_//g; s/_bullseye\.arm64.*//g')
-arm64_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_bullseye.arm64.deb"
+webVer=$(curl -s https://sonic-pi.net | grep -m 1 arm64.deb | sed 's/.*sonic-pi_//g; s/_bookworm\.arm64.*//g')
+arm64_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_bookworm.arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
