7c2e67fc827328db8b13c1c53d73427e65a418d5
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-11-25 22:52:22 -0500

    Tor: update to 13.0.3
    correct auto-updater with new naming scheme

--- .github/workflows/updates/Tor.sh
+++ .github/workflows/updates/Tor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-webVer="$(wget -qO- https://sourceforge.net/projects/tor-browser-ports/files | grep -F 'Download Latest Version' --after 1 | tail -n -1 | tr '<>\-_' '\n' | sed -n 8p)"
-armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}_ALL.tar.xz/download"
-arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-arm64-${webVer}_ALL.tar.xz/download"
+webVer="$(wget -qO- https://sourceforge.net/projects/tor-browser-ports/files | grep -F 'Download Latest Version' --after 1 | tail -n -1 | tr '<>\-_' '\n' | sed -n 8p | sed 's/.tar.xz.*//g')"
+armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}.tar.xz/download"
+arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-arm64-${webVer}.tar.xz/download"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
