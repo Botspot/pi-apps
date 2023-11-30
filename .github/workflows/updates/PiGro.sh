d0ace6343b260f8030bb3baa22642ad0507d326a
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-11-30 17:38:30 -0500

    update pigro auto updater

--- .github/workflows/updates/PiGro.sh
+++ .github/workflows/updates/PiGro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 webVer=$(get_release_raw actionschnitzel/PiGro-Aid-)
-all_url="https://github.com/actionschnitzel/PiGro-Aid-/tree/$webVer"
+all_url="https://github.com/actionschnitzel/PiGro-Aid-/releases/download/${webVer}/pigro-jci-${webVer}.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
