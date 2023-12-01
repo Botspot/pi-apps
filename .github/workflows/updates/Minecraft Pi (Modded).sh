11bae66ea746df4a17d5349f18cb15f057e1f907
TheBrokenRail <connor24nolan@live.com>
2023-12-01 02:26:18 -0500

    Switch MCPI-Reborn To Gitea

--- .github/workflows/updates/Minecraft Pi (Modded).sh
+++ .github/workflows/updates/Minecraft Pi (Modded).sh
@@ -2,17 +2,10 @@
 
 # To avoid writing a custom updater script for this application, we will combine the separate version IDs used in the one URL as one large version variable.
 
-# Build ID
-# This obtains the latest successful build ID.
-build_id="$(curl -s --connect-timeout 15 https://jenkins.thebrokenrail.com/job/minecraft-pi-reborn/job/master/lastStableBuild/buildNumber)"
+# Version
+webVer="$(curl -s --connect-timeout 15 -X 'GET' 'https://gitea.thebrokenrail.com/api/v1/repos/minecraft-pi-reborn/minecraft-pi-reborn/releases/latest' -H 'accept: application/json' | jq --compact-output --raw-output '.tag_name')"
 
-# Version Number
-# This obtains the latest successful build's version number.
-deb_id="$(curl -s --connect-timeout 15 "https://jenkins.thebrokenrail.com/job/minecraft-pi-reborn/job/master/${build_id}/api/json?tree=artifacts%5BrelativePath%5D" | jq | grep 'client-.*-arm64.AppImage"' | sed 's/.*client-//g' | sed 's/-arm64.*//g')"
-
-# Update
-[ ! -z "$build_id" ] && [ ! -z "$deb_id" ] && webVer="${build_id}/artifact/out/minecraft-pi-reborn-client-${deb_id}"
 # This URL is only used for validation.
-armhf_url="https://jenkins.thebrokenrail.com/job/minecraft-pi-reborn/job/master/${webVer}-armhf.AppImage"
-arm64_url="https://jenkins.thebrokenrail.com/job/minecraft-pi-reborn/job/master/${webVer}-arm64.AppImage"
+armhf_url="https://gitea.thebrokenrail.com/minecraft-pi-reborn/minecraft-pi-reborn/releases/download/${webVer}/minecraft-pi-reborn-client-${webVer}-armhf.AppImage"
+arm64_url="https://gitea.thebrokenrail.com/minecraft-pi-reborn/minecraft-pi-reborn/releases/download/${webVer}/minecraft-pi-reborn-client-${webVer}-arm64.AppImage"
 source "${GITHUB_WORKSPACE}/.github/workflows/update_github_script.sh"
