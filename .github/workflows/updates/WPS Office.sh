d49a8839398b161e5c48322b629e0d7c7fc879ed
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-01-15 12:18:09 -0500

    WPS Office: add custom download command for install scripts and URL validation in updater

--- .github/workflows/updates/WPS Office.sh
+++ .github/workflows/updates/WPS Office.sh
@@ -4,4 +4,14 @@ webVer="$(wget -qO- --timeout=5 https://linux.wps.cn/ | grep -o 'wps\-office_.*_
 arm64_url="https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/$(echo "$webVer" | sed 's/.*\.//g')/wps-office_${webVer}_arm64.deb"
 armhf_url="https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/$(echo "$webVer" | sed 's/.*\.//g')/wps-office_${webVer}_arm64.deb"
 
+# custom validate url function
+# this will not overwrite the validate_url function created by the main update_apps.yml file for other update scripts
+function validate_url(){
+    if command wget --header="Referer: https://linux.wps.cn/" --timeout=5 -q --spider "$1"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
