3daf8bab41cabd773974da7044337fdd96b03470
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-01-26 10:52:58 -0500

    CI: correct musescore auto updater again

--- .github/workflows/updates/MuseScore.sh
+++ .github/workflows/updates/MuseScore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(curl -s --header "Authorization: token $GH_PERSONAL_ACCESS_TOKEN" "https://api.github.com/repos/musescore/MuseScore/releases/latest" | jq -r '.assets.[].browser_download_url' | grep aarch64.AppImage$ | sed 's;https://github.com/musescore/MuseScore/releases/download/v'.*'/MuseScore-;;g' | sed 's;-aarch64.AppImage;;g')
+webVer=$(curl -s --header "Authorization: token $GH_PERSONAL_ACCESS_TOKEN" "https://api.github.com/repos/musescore/MuseScore/releases/latest" | jq -r '.assets | .[].browser_download_url' | grep aarch64.AppImage$ | sed 's;https://github.com/musescore/MuseScore/releases/download/v'.*'/MuseScore-;;g' | sed 's;-aarch64.AppImage;;g')
 armhf_url="https://github.com/musescore/MuseScore/releases/download/v${webVer%.*}/MuseScore-${webVer}-armv7l.AppImage"
 arm64_url="https://github.com/musescore/MuseScore/releases/download/v${webVer%.*}/MuseScore-${webVer}-aarch64.AppImage"
 
