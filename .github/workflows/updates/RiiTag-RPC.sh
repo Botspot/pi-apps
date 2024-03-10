351491f84c5efc1f5e970395e7215f051e7c176c
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-03-09 19:43:21 -0500

    RiiTag-RPC: update to 1.3.5
    upstream instability https://github.com/RiiConnect24/RiiTag-RPC/issues/43
    
    partial revert of https://github.com/Botspot/pi-apps/commit/0a3f1a3f714c2483dd1caec821e05390edb34a37

--- .github/workflows/updates/RiiTag-RPC.sh
+++ .github/workflows/updates/RiiTag-RPC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 webVer=$(get_release_raw RiiConnect24/riitag-rpc)
-arm64_url="https://github.com/riiconnect24/riitag-rpc/releases/download/${webVer}/RiiTag-RPC_Rpi4B"
+arm64_url="https://github.com/riiconnect24/riitag-rpc/releases/download/${webVer}/rpi-4b-aarch64.zip"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
