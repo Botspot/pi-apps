33f6353790db3ac9e62f6065c8e6acfdf4cf9563
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-06-01 20:27:06 -0400

    eDEX-UI: remove auto-updater

--- .github/workflows/updates/eDEX-UI.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-webVer=$(get_release GitSquared/edex-ui)
-armhf_url="https://github.com/GitSquared/edex-ui/releases/download/v${webVer}/eDEX-UI-Linux-armv7l.AppImage"
-arm64_url="https://github.com/GitSquared/edex-ui/releases/download/v${webVer}/eDEX-UI-Linux-arm64.AppImage"
-
-source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
