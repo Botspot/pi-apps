c5dd1da9ddfffc02b9422481dbdbcc12c1119e25
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-06-05 16:15:02 -0400

    Better Chromium: add auto updater warning on failure

--- .github/workflows/updates/Better Chromium.sh
+++ .github/workflows/updates/Better Chromium.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # enable chromium-debs repo workflow (disables automatically after 60 days inactivity)
-GH_TOKEN="$GH_PERSONAL_ACCESS_TOKEN" gh --repo Pi-Apps-Coders/chromium-debs workflow enable Update-Chromium.yml || true
+GH_TOKEN="$GH_PERSONAL_ACCESS_TOKEN" gh --repo Pi-Apps-Coders/chromium-debs workflow enable Update-Chromium.yml || warning "Failed to enable Update-Chromium action in Pi-Apps-Coders/chromium-debs"
