01341319c17bb0670978eef865c7f8a348209663
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-07-29 17:06:01 -0400

    jGRASP IDE: update auto-updater
    thanks to Larry for the version URL and documentation provided

--- .github/workflows/updates/jGRASP IDE.sh
+++ .github/workflows/updates/jGRASP IDE.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-version=$(curl -Ls 'https://spider.eng.auburn.edu/user-cgi/grasp/grasp.pl?;dl=download_jgrasp.html' | grep "jGRASP - Version" | sed 's/.*jGRASP - Version //' | sed 's/<br>//' | tr -d .)
+# The format of the returned data is:
+#    final;last_major;location:final_bundled;last_major;location:non-final;last_major;location:non-final_bundled;last_major;location
+# if there is a non-final release available 
+# 
+# and:
+#    final;last_major;location:final_bundled;last_major;location
+# if there is only a final release available.
+
+version=$(wget -qO- https://spider.eng.auburn.edu/user-cgi/grasp/version7.pl | awk -F '[;:]' '{ print $3 } { if ($7) print $9 }' | tail -n 1 | sed 's;/dl4g/jgrasp/jgrasp;;g')
 
 all_url="https://www.jgrasp.org/dl4g/jgrasp/jgrasp${version}.zip"
 
