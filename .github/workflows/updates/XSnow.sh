0a202ca494d514badec7d7f0be0a7a8d29949c26
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-09-13 16:01:11 -0400

    XSnow: update auto-updater

--- .github/workflows/updates/XSnow.sh
+++ .github/workflows/updates/XSnow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(wget -qO- https://www.ratrabbit.nl/ratrabbit/xsnow/downloads/index.html | grep -A1 "Download source:" | tail -n 1  | sed 's/<p>//g ; s+</p>++g' | sed 's+"./../../../downloads/xsnow/xsnow-.*tar.gz">++g ; s+</a>++g' | grep -oP '^[^-]*\-\K[^*]+' | sed -n 's/.tar.gz//p')
+webVer=$(wget -qO- https://www.ratrabbit.nl/ratrabbit/xsnow/downloads/index.html | grep -A1 "Current version:" | tail -n 1  | sed 's/<p>//g ; s+</p>++g' | sed 's+"./../../../downloads/xsnow/xsnow-.*tar.gz">++g ; s+</a>++g' | grep -oP '^[^-]*\-\K[^*]+' | sed -n 's/.tar.gz//p')
 all_url="https://www.ratrabbit.nl/downloads/xsnow/xsnow-${webVer}.tar.gz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
