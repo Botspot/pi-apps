cce93501fc53677b4d3a5d9c8a7d59313bbeed1e
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-10-12 19:13:38 -0400

    tldr: update to 1.7.0
    also switch to aarch64 build instead of armv7l build (the aarch64 was not available when the auto-updater was first implemented)
    
    also switch to new repo location in all other locations

--- .github/workflows/updates/tldr.sh
+++ .github/workflows/updates/tldr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 webVer=$(get_release tealdeer-rs/tealdeer)
-armhf_url="https://github.com/dbrgn/tealdeer/releases/download/v${webVer}/tealdeer-linux-arm-musleabihf"
-arm64_url="https://github.com/dbrgn/tealdeer/releases/download/v${webVer}/tealdeer-linux-armv7-musleabihf"
+armhf_url="https://github.com/tealdeer-rs/tealdeer/releases/download/v${webVer}/tealdeer-linux-arm-musleabihf"
+arm64_url="https://github.com/tealdeer-rs/tealdeer/releases/download/v${webVer}/tealdeer-linux-aarch64-musl"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
