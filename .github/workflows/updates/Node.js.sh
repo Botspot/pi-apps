f22d19936284afa599089e87ec1cd5482b43ec67
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-11-04 16:08:04 -0500

    CI: change how Node.js version is checked

--- .github/workflows/updates/Node.js.sh
+++ .github/workflows/updates/Node.js.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-webVer[0]=$(wget -qO- https://nodejs.org/en/download/prebuilt-binaries/ | tr '<>' '\n' | grep 'https://nodejs\.org/dist/.*\.tar\.gz' -m1 | tr '/' '\n' | grep '^v')
-armhf_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-armv7l.tar.xz"
-arm64_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-arm64.tar.xz"
-
 webVer[1]=$(get_release_raw nvm-sh/nvm)
 all_url[1]="https://raw.githubusercontent.com/nvm-sh/nvm/${webVer[1]}/install.sh"
+
+# obtain latest lts version of nodejs from nvm to avoid any deployment lag
+# previously the version was obtained from the website which could update faster than nvm itself, leaving to pi-apps prompting users to update too early
+# https://github.com/nvm-sh/nvm/issues/3451
+wget -qO nvm.sh "https://raw.githubusercontent.com/nvm-sh/nvm/${webVer[1]}/nvm.sh"
+webVer[0]=$(source ./nvm.sh; nvm version-remote --lts)
+rm -f nvm.sh
+
+armhf_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-armv7l.tar.xz"
+arm64_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-arm64.tar.xz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
