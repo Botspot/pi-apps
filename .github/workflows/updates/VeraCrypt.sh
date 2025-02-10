67ccf1ae7519d2897f28cf150ad0e47723d8c653
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2025-02-09 22:58:02 -0500

    VeraCrypt: remove Buster from automatic updater

--- .github/workflows/updates/VeraCrypt.sh
+++ .github/workflows/updates/VeraCrypt.sh
@@ -2,12 +2,9 @@
 
 webVer[0]="$(get_release_raw veracrypt/VeraCrypt | sed 's/VeraCrypt_//g' )"
 webVer[1]="${webVer[0]}"
-webVer[2]="${webVer[0]}"
-armhf_url[0]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[0]}/veracrypt-${webVer[0]}-Debian-10-armhf.deb"
-arm64_url[0]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[0]}/veracrypt-${webVer[0]}-Debian-10-arm64.deb"
-armhf_url[1]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[1]}/veracrypt-${webVer[1]}-Debian-11-armhf.deb"
-arm64_url[1]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[1]}/veracrypt-${webVer[1]}-Debian-11-arm64.deb"
-armhf_url[2]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[2]}/veracrypt-${webVer[2]}-Debian-12-armhf.deb"
-arm64_url[2]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[2]}/veracrypt-${webVer[2]}-Debian-12-arm64.deb"
+armhf_url[1]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[0]}/veracrypt-${webVer[0]}-Debian-11-armhf.deb"
+arm64_url[1]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[0]}/veracrypt-${webVer[0]}-Debian-11-arm64.deb"
+armhf_url[2]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[1]}/veracrypt-${webVer[1]}-Debian-12-armhf.deb"
+arm64_url[2]="https://github.com/veracrypt/VeraCrypt/releases/download/VeraCrypt_${webVer[1]}/veracrypt-${webVer[1]}-Debian-12-arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
