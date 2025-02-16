cf844f14427d76d6708bd47f3b83add776115478
Botspot <54716352+Botspot@users.noreply.github.com>
2025-02-16 16:41:08 -0600

    Add Systemd Pilot

---
+++ .github/workflows/updates/Systemd Pilot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+version=$(get_release mfat/systemd-pilot)
+armhf_url="https://github.com/mfat/systemd-pilot/releases/download/v3.0/systemd-pilot_${version}-1_all.deb"
+arm64_url="https://github.com/mfat/systemd-pilot/releases/download/v3.0/systemd-pilot_${version}-1_all.deb"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
