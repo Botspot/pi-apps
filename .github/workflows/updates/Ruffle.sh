2912ddb5726fb91a1934d00c68f3deea04368040
matu6968 <wierzejskimateusz8@gmail.com>
2025-03-09 01:46:50 +0100

    forgot to put modified github workflow

--- .github/workflows/updates/Ruffle.sh
+++ .github/workflows/updates/Ruffle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 version=$(get_prerelease ruffle-rs/ruffle)
-all_url="https://github.com/ruffle-rs/ruffle/releases/download/${version}/ruffle-${version}-reproducible-source.zip"
+all_url="https://github.com/ruffle-rs/ruffle/tree/$version"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
