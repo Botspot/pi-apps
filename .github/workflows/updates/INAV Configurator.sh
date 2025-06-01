7a2a899f8624d69f7b3479288be3cc77c5032bb5
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2025-06-01 16:05:51 -0400

    INAV Configurator: switch to stable release

--- .github/workflows/updates/INAV Configurator.sh
+++ .github/workflows/updates/INAV Configurator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=$(get_release iNavFlight/inav-configurator-nightly)
-arm64_url="https://github.com/iNavFlight/inav-configurator-nightly/releases/download/${version}/INAV-Configurator_linux_arm64_8.0.0.deb"
+version=$(get_release iNavFlight/inav-configurator)
+arm64_url="https://github.com/iNavFlight/inav-configurator/releases/download/${version}/INAV-Configurator_linux_arm64_${version}.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
