13327a39fcbe4980871a57fb3d56c97e7e2a0e62
techguy16 <88870951+techguy16@users.noreply.github.com>
2025-07-23 13:52:36 +1200

    LibreWolf now publishing `xz` files instead of `bz2`

--- .github/workflows/updates/LibreWolf.sh
+++ .github/workflows/updates/LibreWolf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(curl -s "https://gitlab.com/api/v4/projects/44042130/releases" | tr '{},[]' '\n' | grep -x '"direct_asset_url":.*linux-arm64-package.tar.bz2"' -m1 | sed 's+.*/packages/generic/librewolf/++g ; s+/.*++g')
-arm64_url="https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/${webVer}/librewolf-${webVer}-linux-arm64-package.tar.bz2"
+webVer=$(curl -s "https://gitlab.com/api/v4/projects/44042130/releases" | tr '{},[]' '\n' | grep -x '"direct_asset_url":.*linux-arm64-package.tar.xz"' -m1 | sed 's+.*/packages/generic/librewolf/++g ; s+/.*++g')
+arm64_url="https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/${webVer}/librewolf-${webVer}-linux-arm64-package.tar.xz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
