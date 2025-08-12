38d040a85a4817aa7981ad500a581c45d2d29c1d
Botspot <54716352+Botspot@users.noreply.github.com>
2025-08-12 12:13:34 -0400

    Update FreeTube
    url naming scheme added _beta, preventing auto-updater from working

--- .github/workflows/updates/FreeTube.sh
+++ .github/workflows/updates/FreeTube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 webVer=$(get_prerelease FreeTubeApp/FreeTube)
-armhf_url="https://github.com/FreeTubeApp/FreeTube/releases/download/v${webVer}/freetube_${webVer%-beta}_armv7l.deb"
-arm64_url="https://github.com/FreeTubeApp/FreeTube/releases/download/v${webVer}/freetube_${webVer%-beta}_arm64.deb"
+armhf_url="https://github.com/FreeTubeApp/FreeTube/releases/download/v${webVer}/freetube_${webVer%-beta}_beta_armv7l.deb"
+arm64_url="https://github.com/FreeTubeApp/FreeTube/releases/download/v${webVer}/freetube_${webVer%-beta}_beta_arm64.deb"
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
