c55d0cfbda9e42e451c6258a7d6f50d417ee9714
Botspot <54716352+Botspot@users.noreply.github.com>
2025-09-17 15:47:52 -0400

    Fix Boxy SVG to close #2830

--- .github/workflows/updates/Boxy SVG.sh
+++ .github/workflows/updates/Boxy SVG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=$(wget -qO- https://raw.githubusercontent.com/flathub/com.boxy_svg.BoxySVG/master/com.boxy_svg.BoxySVG.yaml | grep 'url: https://storage.boxy-svg.com/flathub' | sed 's/.*url: //g'  | tr '?-' '\n' | grep '\.zip$')
-all_url="https://storage.boxy-svg.com/flathub/app-${version}"
+version=$(wget -qO- https://raw.githubusercontent.com/flathub/com.boxy_svg.BoxySVG/master/com.boxy_svg.BoxySVG.yaml | grep 'https://storage\.boxy-svg\.com/builds/.*arm64\.zip$' -o | awk -F- '{print $4}')
+all_url="https://storage.boxy-svg.com/builds/boxy-svg-${version}-linux-arm64.zip"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
