90f2a21bfd152501a2f266592781abb5786192fe
Botspot <54716352+Botspot@users.noreply.github.com>
2025-09-17 17:37:41 -0400

    Floorp: Use tar.xz now and update to latest version

--- .github/workflows/updates/Floorp.sh
+++ .github/workflows/updates/Floorp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webVer=$(get_release Floorp-Projects/Floorp)
-arm64_url="https://github.com/Floorp-Projects/Floorp/releases/download/v${webVer}/floorp-${webVer}.linux-aarch64.tar.bz2"
+version=$(get_release Floorp-Projects/Floorp)
+arm64_url="https://github.com/Floorp-Projects/Floorp/releases/download/v${version}/floorp-linux-aarch64.tar.xz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
