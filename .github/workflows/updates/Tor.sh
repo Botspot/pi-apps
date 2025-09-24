1f7a367648cd6a7374aef4d78a7414a9bdb3d62a
Vlad <150745492+ooovlad@users.noreply.github.com>
2025-09-24 21:45:33 +0000

    Tor <- merge Tor Browser Nightly

--- .github/workflows/updates/Tor.sh
+++ .github/workflows/updates/Tor.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-
-webVer="$(wget -qO- https://sourceforge.net/projects/tor-browser-ports/files | grep -F 'Download Latest Version' --after 1 | tail -n -1 | tr '<>\-_' '\n' | sed -n 8p | sed 's/\.tar\.xz.*//g')"
-armhf_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-armhf-${webVer}.tar.xz/download"
-arm64_url="https://sourceforge.net/projects/tor-browser-ports/files/${webVer}/tor-browser-linux-arm64-${webVer}.tar.xz/download"
+# Extract versions from the specific structure:
+# Lines look like:
+# <img ...> <a href="tbb-nightly.2025.09.24/">tbb-nightly.2025.09.24/</a> 2025-09-24 14:28    -
+# Then sort by YYYYMMDD numerically to be portable (no reliance on sort -V).
+webVer="$(
+  wget -qO- "https://nightlies.tbb.torproject.org/nightly-builds/tor-browser-builds" 2>/dev/null \
+  | sed -nE 's@.*href="tbb-nightly\.([0-9]{4}\.[0-9]{2}\.[0-9]{2})/".*@\1@p' \
+  | awk -F. 'NF==3 { printf "%s %s%s%s\n", $0, $1, $2, $3 }' \
+  | LC_ALL=C sort -k2,2n \
+  | tail -n 1 \
+  | cut -d' ' -f1
+)"
+arm64_url="https://nightlies.tbb.torproject.org/nightly-builds/tor-browser-builds/tbb-nightly.$webVer/nightly-linux-aarch64/tor-browser-linux-aarch64-tbb-nightly.$webVer.tar.xz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
