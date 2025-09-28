3ceb6234c516f2ec128a9106efaf3fd1c20c0b4c
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2025-09-27 21:36:13 -0400

    Renoise (Demo): remove 32bit version as has not existed upstream for a while

--- .github/workflows/updates/Renoise (Demo).sh
+++ .github/workflows/updates/Renoise (Demo).sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-webVer="$(curl -s https://files.renoise.com/demo/ | grep "armhf" | sed -e 's/<a href="//' -e 's/".*//' -e 's/Renoise_//' -e 's/_Demo_Linux_armhf.tar.gz//')"
-armhf_url="https://files.renoise.com/demo/Renoise_${webVer}_Demo_Linux_armhf.tar.gz"
-arm64_url="https://files.renoise.com/demo/Renoise_${webVer}_Demo_Linux_arm64.tar.gz"
+version="$(curl -s https://files.renoise.com/demo/ | grep "arm64" | sed -e 's/<a href="//' -e 's/".*//' -e 's/Renoise_//' -e 's/_Demo_Linux_arm64.tar.gz//' | tail -1)"
+arm64_url="https://files.renoise.com/demo/Renoise_${version}_Demo_Linux_arm64.tar.gz"
 
 source ${GITHUB_WORKSPACE}/.github/workflows/update_github_script.sh
