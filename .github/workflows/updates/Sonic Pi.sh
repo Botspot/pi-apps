76a619182ce897a57b668c5d4d6831c9b7f7316b
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2025-10-16 17:37:42 -0400

    Sonic Pi: fix updater and add trixie

--- .github/workflows/updates/Sonic Pi.sh
+++ .github/workflows/updates/Sonic Pi.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-webVer=$(curl -s https://sonic-pi.net | grep -m 1 arm64.deb | sed 's/.*sonic-pi_//g; s/_bookworm\.arm64.*//g')
-arm64_url="https://sonic-pi.net/files/releases/v${webVer%_*}/sonic-pi_${webVer}_bookworm.arm64.deb"
+version[0]=$(curl -sL https://github.com/sonic-pi-net/sonic-pi/releases/latest | grep -m 1 bookworm.arm64.deb | sed 's/.*sonic-pi_//g; s/_bookworm\.arm64.*//g')
+version[1]=$(curl -sL https://github.com/sonic-pi-net/sonic-pi/releases/latest | grep -m 1 trixie.arm64.deb | sed 's/.*sonic-pi_//g; s/_trixie\.arm64.*//g')
+arm64_url[0]="https://sonic-pi.net/files/releases/v${version[0]%_*}/sonic-pi_${version[0]}_bookworm.arm64.deb"
+arm64_url[1]="https://sonic-pi.net/files/releases/v${version[1]%_*}/sonic-pi_${version[1]}_trixie.arm64.deb"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
