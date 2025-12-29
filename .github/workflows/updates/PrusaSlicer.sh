30d67f3631b7e31e7a623023fdede272534b021f
Botspot <54716352+Botspot@users.noreply.github.com>
2025-12-29 14:46:28 -0500

    prusaslicer dropped armhf builds, so lock version

--- .github/workflows/updates/PrusaSlicer.sh
+++ .github/workflows/updates/PrusaSlicer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version=$(get_release davidk/PrusaSlicer-ARM.AppImage | sed "s/ersion_//g")
-armhf_url="https://github.com/davidk/PrusaSlicer-ARM.AppImage/releases/download/version_${version}/PrusaSlicer-version_${version}-armhf.AppImage"
+#armhf_url="https://github.com/davidk/PrusaSlicer-ARM.AppImage/releases/download/version_${version}/PrusaSlicer-version_${version}-armhf.AppImage"
 arm64_url="https://github.com/davidk/PrusaSlicer-ARM.AppImage/releases/download/version_${version}/PrusaSlicer-${version}-aarch64-full.AppImage"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
