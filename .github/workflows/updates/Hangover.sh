aaf73be1a10698a9822a5b1c11192a85d23cf678
Botspot <54716352+Botspot@users.noreply.github.com>
2025-12-28 23:37:03 -0500

    Update Hangover and add updater script

---
+++ .github/workflows/updates/Hangover.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+version=$(get_prerelease AndreRH/hangover)
+arm64_url="https://github.com/AndreRH/hangover/releases/download/${version}/$(echo "$version" | sed 's/hangover-/hangover_/g')}_debian13_trixie_arm64.tar"
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
