4879a064eec7acbe79a4dd07a8f7e11b7375570e
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2026-01-18 18:03:34 -0500

    Node.js: fix armhf to use v22 lts and arm64 to use latest lts
    this fixes the commits from @Botspot that do not work properly:
    
    https://github.com/Botspot/pi-apps/commit/8e6cc9e56137d5d9056f5bc4906aeaf731ab2ea2
    https://github.com/Botspot/pi-apps/commit/6119152d12aa354afe0d3251b4762ec8e9db16b5
    
    also uses explicit version numbers during installation so installation is deterministic and not based on when the user installs

--- .github/workflows/updates/Node.js.sh
+++ .github/workflows/updates/Node.js.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-webVer[1]=$(get_release_raw nvm-sh/nvm)
-all_url[1]="https://raw.githubusercontent.com/nvm-sh/nvm/${webVer[1]}/install.sh"
+version[1]=$(get_release_raw nvm-sh/nvm)
+armhf_url[1]="https://raw.githubusercontent.com/nvm-sh/nvm/${version[1]}/install.sh"
 
 # obtain latest lts version of nodejs from nvm to avoid any deployment lag
 # previously the version was obtained from the website which could update faster than nvm itself, leaving to pi-apps prompting users to update too early
 # https://github.com/nvm-sh/nvm/issues/3451
-wget -qO nvm.sh "https://raw.githubusercontent.com/nvm-sh/nvm/${webVer[1]}/nvm.sh"
-webVer[0]=$(source ./nvm.sh; nvm version-remote --lts)
+wget -qO nvm.sh "https://raw.githubusercontent.com/nvm-sh/nvm/${version[1]}/nvm.sh"
+# v22 release is the last to support ARMhf and is codename "Jod"
+version[0]=$(source ./nvm.sh; nvm version-remote --lts=Jod)
 rm -f nvm.sh
 
-armhf_ver=$(source ./nvm.sh; nvm version-remote --lts)
-armhf_url[0]="https://nodejs.org/dist/${armhf_ver}/node-${armhf_ver}-linux-armv7l.tar.xz"
-arm64_url[0]="https://nodejs.org/dist/${webVer}/node-${webVer}-linux-arm64.tar.xz"
+armhf_url[0]="https://nodejs.org/dist/${version[0]}/node-${version[0]}-linux-armv7l.tar.xz"
+
+source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
+
+
+version[1]=$(get_release_raw nvm-sh/nvm)
+arm64_url[1]="https://raw.githubusercontent.com/nvm-sh/nvm/${version[1]}/install.sh"
+
+wget -qO nvm.sh "https://raw.githubusercontent.com/nvm-sh/nvm/${version[1]}/nvm.sh"
+# the latest LTS supports ARM64
+version[0]=$(source ./nvm.sh; nvm version-remote --lts)
+rm -f nvm.sh
+
+arm64_url[0]="https://nodejs.org/dist/${version[0]}/node-${version[0]}-linux-arm64.tar.xz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
