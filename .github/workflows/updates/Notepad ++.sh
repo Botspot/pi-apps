74efa7b92c0bfd5d60e5a1b0b2581a7ad4430a82
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2026-03-28 17:53:45 -0400

    Notepad++: fix CI automatic updater

--- .github/workflows/updates/Notepad ++.sh
+++ .github/workflows/updates/Notepad ++.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-webVer=$(get_release notepad-plus-plus/notepad-plus-plus)
-armhf_url="https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v${webVer}/npp.${webVer}.portable.zip"
+version=$(get_release notepad-plus-plus/notepad-plus-plus)
+armhf_url="https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v${version}/npp.${version}.portable.zip"
+arm64_url="https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v${version}/npp.${version}.portable.x64.zip"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
