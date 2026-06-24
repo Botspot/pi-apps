f6002a7c799f19428c6e62f221c8f0321805eca4
Botspot <54716352+Botspot@users.noreply.github.com>
2026-06-24 00:08:39 -0500

    jgrasp auto-updater: no check certificate

--- .github/workflows/updates/jGRASP IDE.sh
+++ .github/workflows/updates/jGRASP IDE.sh
@@ -8,7 +8,7 @@
 #    final;last_major;location:final_bundled;last_major;location
 # if there is only a final release available.
 
-version=$(wget -qO- https://spider.eng.auburn.edu/user-cgi/grasp/version7.pl | awk -F '[;:]' '{ print $3 } { if ($7) print $9 }' | tail -n 1 | sed 's;/dl4g/jgrasp/jgrasp;;g')
+version=$(wget --no-check-certificate -qO- https://spider.eng.auburn.edu/user-cgi/grasp/version7.pl | awk -F '[;:]' '{ print $3 } { if ($7) print $9 }' | tail -n 1 | sed 's;/dl4g/jgrasp/jgrasp;;g')
 
 all_url="https://www.jgrasp.org/dl4g/jgrasp/jgrasp${version}.zip"
 
