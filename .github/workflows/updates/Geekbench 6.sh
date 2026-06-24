9d17de04fd6a8e0311a71e191b36595eb4b2965c
Botspot <54716352+Botspot@users.noreply.github.com>
2026-06-24 00:24:32 -0500

    geekbench updater: bypass cloudflare anti-bot

--- .github/workflows/updates/Geekbench 6.sh
+++ .github/workflows/updates/Geekbench 6.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-webVer=$(wget -qO- https://www.geekbench.com/download/linux/ | grep "for Linux. If your download does not start in"  | sed "s/ for Linux.*//g ; s/.* //g")
-arm64_url="https://cdn.geekbench.com/Geekbench-${webVer}-LinuxARMPreview.tar.gz"
+#version=$(wget -qO- https://www.geekbench.com/download/linux/ | grep "for Linux. If your download does not start in"  | sed "s/ for Linux.*//g ; s/.* //g")
+version=$(wget -qO- "https://formulae.brew.sh/api/cask/geekbench.json" | tr ':,' '\n' | grep -x '"version"' -A1 | grep '^"6\.' | tr -d '"')
+arm64_url="https://cdn.geekbench.com/Geekbench-${version}-LinuxARMPreview.tar.gz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
