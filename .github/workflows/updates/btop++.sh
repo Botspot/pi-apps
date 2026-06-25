43f95cbce8be7f345bc7f31ff3b797723c9465aa
Botspot <54716352+Botspot@users.noreply.github.com>
2026-06-24 20:35:01 -0500

    btop++: change to tar.gz and update to 1.4.7

--- .github/workflows/updates/btop++.sh
+++ .github/workflows/updates/btop++.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 webVer=$(get_release aristocratos/btop)
-armhf_url="https://github.com/aristocratos/btop/releases/download/v${webVer}/btop-armv7-unknown-linux-musleabi.tbz"
-arm64_url="https://github.com/aristocratos/btop/releases/download/v${webVer}/btop-aarch64-unknown-linux-musl.tbz"
+armhf_url="https://github.com/aristocratos/btop/releases/download/v${webVer}/btop-armv7-unknown-linux-musleabi.tar.gz"
+arm64_url="https://github.com/aristocratos/btop/releases/download/v${webVer}/btop-aarch64-unknown-linux-musl.tar.gz"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
