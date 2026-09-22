2a34a4c80119ecdb7fe4013f9aa325b72398a234
Botspot <54716352+Botspot@users.noreply.github.com>
2026-09-21 22:16:03 -0500

    vara fm, vara hf, winlink express: use user agent everywhere

--- .github/workflows/updates/VARA FM.sh
+++ .github/workflows/updates/VARA FM.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-version="$(wget -qO- --user-agent="Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36" https://downloads.winlink.org/VARA%20Products/ | sed 's/<br>/\n/g' | grep 'VARA%20FM' | sed 's/">.*//g ; s/.*="\///g' | sort -V | tail -1)"
+version="$(wget -qO- --header="User-Agent: This download is a result of the Pi-Apps store. Please stop blocking us. Please contact us if you have any questions. https://github.com/Botspot/pi-apps/issues/new/choose" https://downloads.winlink.org/VARA%20Products/ | sed 's/<br>/\n/g' | grep 'VARA%20FM' | sed 's/">.*//g ; s/.*="\///g' | sort -V | tail -1)"
 all_url="https://downloads.winlink.org/$version"
+
+# custom validate url function
+# this will not overwrite the validate_url function created by the main update_apps.yml file for other update scripts
+function validate_url(){
+    if command wget -t 5 --waitretry=10 --timeout=10 --retry-connrefused -q --spider --header="User-Agent: This download is a result of the Pi-Apps store. Please stop blocking us. Please contact us if you have any questions. https://github.com/Botspot/pi-apps/issues/new/choose" "$1"; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
