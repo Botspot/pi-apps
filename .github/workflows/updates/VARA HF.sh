6591e0d92d01cb31a0e063bfad78e66a35451603
Botspot <54716352+Botspot@users.noreply.github.com>
2026-09-21 21:38:52 -0500

    add user agent to winlink express, vara fm, and vara hf

--- .github/workflows/updates/VARA HF.sh
+++ .github/workflows/updates/VARA HF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="$(wget -qO- https://downloads.winlink.org/VARA%20Products/ | sed 's/<br>/\n/g' | grep 4.9.0 | sed 's/">.*//g ; s/.*="\///g')"
+version="$(wget -qO- --user-agent="Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36" https://downloads.winlink.org/VARA%20Products/ | sed 's/<br>/\n/g' | grep 4.9.0 | sed 's/">.*//g ; s/.*="\///g')"
 all_url="https://downloads.winlink.org/$version"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
--- .github/workflows/updates/Winlink Express.sh
+++ .github/workflows/updates/Winlink Express.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="$(wget -qO- https://downloads.winlink.org/User%20Programs/ | sed 's/<br>/\n/g' | grep 'Winlink_Express_install' | sed 's/">.*//g ; s/.*="\///g' | sort -V | tail -1)"
+version="$(wget -qO- --user-agent="Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36" https://downloads.winlink.org/User%20Programs/ | sed 's/<br>/\n/g' | grep 'Winlink_Express_install' | sed 's/">.*//g ; s/.*="\///g' | sort -V | tail -1)"
 all_url="https://downloads.winlink.org/$version"
 
 source $GITHUB_WORKSPACE/.github/workflows/update_github_script.sh
